f348acc28674fccefbebf899efdffe801372760b
jackie <jackie@playmore.com.hk>
2023-10-14 11:51:56 +0800

    init

---
+++ Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by Jackie Lu on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
