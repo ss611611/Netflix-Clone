a9e9bbd1b390f94b119d005af2ee3ca05e6e6a3b
ss611611 <ss611611@gmail.com>
2023-11-04 20:58:01 +0800

    Creating TitlePreviewViewController

--- Netflix-Clone/Controllers/Core/HomeViewController.swift
+++ Netflix-Clone/Controllers/Core/HomeViewController.swift
@@ -19,6 +19,9 @@ enum Sections: Int {
 class HomeViewController: UIViewController {
     
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
     let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular",  "Upcoming Movies", "Top rated"]
 
     private let homeFeedTable: UITableView = {
@@ -36,9 +39,25 @@ class HomeViewController: UIViewController {
         
         confiqureNavber()
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
         homeFeedTable.tableHeaderView = headerView
+        configureHeaderView()
         
+    }
+    
+    private func configureHeaderView() {
+        
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func confiqureNavber() {
